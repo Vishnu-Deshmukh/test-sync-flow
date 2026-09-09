6a648b826f14d0cb1426b2f72918744c8acd2067
Vishnu Deshmukh <vishnudeshmukh@Vishnu-Deshmukh.local>
2026-09-09 15:18:51 +0530

    add space

--- ATLNodeBase.py
+++ ATLNodeBase.py
@@ -59,6 +59,9 @@ class ATLNodeBase(AgentBase):
         Returns:
             str: The formatted start message
         """
+
+
+        
         message_map = {
             "setup": AnalyseStepMessages.SETUP_START,
             "run_in_thread": AnalyseStepMessages.RUN_START, 
