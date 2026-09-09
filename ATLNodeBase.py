795bbe4ac1ae66e77251e09d1305ebfdb64a6be0
Vishnu Deshmukh <vishnudeshmukh@Vishnu-Deshmukh.local>
2026-09-09 22:49:53 +0530

    space push

--- ATLNodeBase.py
+++ ATLNodeBase.py
@@ -61,10 +61,6 @@ class ATLNodeBase(AgentBase):
         step_map = {"setup": "2", "run_in_thread": "2", "run": "3"}
         return step_map.get(func_name, "")
 
-    
-
-
-
 
 
     def _get_start_message(self, func_name: str, agent_name: str) -> str:
