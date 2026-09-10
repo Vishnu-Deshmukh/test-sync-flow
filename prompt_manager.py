6ebe8f726f26c70b0d82935cce778a0d0a6f7349
Vishnu Deshmukh <vishnudeshmukh@Vishnu-Deshmukh.local>
2026-09-10 17:54:28 +0530

    add sapce

--- prompt_manager.py
+++ prompt_manager.py
@@ -23,6 +23,9 @@ class PromptManager:
             self.initialized = True
             self.new = False
 
+
+            
+
     def get_prompt(self, prompt_name: str, **kwargs: Any) -> str:
         """Fetches string prompt from prompt_templates collection."""
         template = self._get_raw_item(prompt_name)
