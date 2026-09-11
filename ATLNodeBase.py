284f0260222d3ca3192c8ea55ce4706d504f4f64
Vishnu Deshmukh <vishnudeshmukh@Vishnu-Deshmukh.local>
2026-09-11 15:16:31 +0530

    remove space

--- ATLNodeBase.py
+++ ATLNodeBase.py
@@ -62,10 +62,6 @@ class ATLNodeBase(AgentBase):
         return step_map.get(func_name, "")
 
 
-
-
-
-
     def _get_start_message(self, func_name: str, agent_name: str) -> str:
         """
         Get the start message for a given function name and agent name.
